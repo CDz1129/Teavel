ee5ae44669981f86377c40abf20766386ae88bb9
CDz <smartcdz@163.com>
2019-08-27 19:16:48 +0800

    swiper

---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,24 @@
+<template>
+<swiper :options="swiperOption">
+    <swiper-slide>
+        <img src="" alt="">
+    </swiper-slide>
+    <swiper-slide>I'm Slide 2</swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>>
