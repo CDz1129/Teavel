31963e79cc4520a7333ba611f47e16f4eb45238d
CDz <smartcdz@163.com>
2019-08-27 18:28:34 +0800

    7-2 home - header

---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe65c;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display: flex
+  line-height: 0.86rem
+  background: $bgColor
+  color: #fff
+  .header-left
+    width: 0.64rem
+    float: left
+    .back-icon
+      text-align: center
+      font-size: 0.4rem
+  .header-input
+    flex: 1
+    height: 0.64rem
+    line-height: 0.64rem
+    margin-top: 0.12rem
+    background: #fff
+    border-radius: 0.1rem
+    padding-left: 0.2rem
+    color: #ccc
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
+    .arrow-icon
+      margin-left: -0.04rem
+      font-size: 0.30rem
+</style>
