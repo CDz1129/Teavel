0f65d96aec9eadcce9bb007061af4ae250257083
cdz <smartcdz@163.com>
2019-08-28 08:41:57 +0800

    img-swiper

--- src/views/home/components/Swiper.vue
+++ src/views/home/components/Swiper.vue
@@ -1,11 +1,21 @@
 <template>
-<swiper :options="swiperOption">
-    <swiper-slide>
-        <img src="" alt="">
-    </swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img
+          class="swiper-img"
+          src="https://z1.muscache.cn/pictures/hongbao/marquee/original/08993cb0-97e9-4bf2-a4ec-f1f7055a7010.jpg"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="swiper-img"
+          src="https://z1.muscache.cn/pictures/hongbao/marquee/original/ca413674-f798-401a-9a6d-c4cd01e67238.jpg"
+        />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -13,12 +23,22 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+
+.wrapper
+  overflow: hidden
+  width: 100%
+  // height: 0
+  // padding-bottom: 31.25%
+  .swiper-img
+    width: 100%
 
 </style>>
