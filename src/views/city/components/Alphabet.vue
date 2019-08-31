675472d769694b8f27c37e4ed756edb9772c5cd4
cdz <smartcdz@163.com>
2019-08-31 19:01:54 +0800

    perfomance

--- src/views/city/components/Alphabet.vue
+++ src/views/city/components/Alphabet.vue
@@ -21,8 +21,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   computed: {
     letters () {
@@ -41,14 +46,21 @@ export default {
       this.touchStatus = true
     },
     handleTouchMove (e) {
+      // 由于touchmove的变动特别快，所以执行此函数会特别的消耗性能，
+      // 对此函数做截流的操作，可以提升其性能
+
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        // eslint-disable-next-line no-console
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          // eslint-disable-next-line no-console
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 5)
       }
     },
     handleTouchEnd () {
