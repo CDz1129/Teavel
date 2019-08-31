2ab26a478930945905468236658d0f8d3dc5f0f7
cdz <smartcdz@163.com>
2019-08-31 18:37:43 +0800

    add city list data by axios and touch city alphabet cities move

---
+++ src/views/city/components/Alphabet.vue
@@ -0,0 +1,76 @@
+<template>
+  <ul class="list">
+    <li
+      class="item"
+      v-for="item in letters"
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleClick"
+    >{{item}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (const item in this.cities) {
+        letters.push(item)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        // eslint-disable-next-line no-console
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+  display: flex
+  flex-direction: column
+  justify-content: center
+  position: absolute
+  top: 1.58rem
+  right: 0
+  bottom: 0
+  width: 0.4rem
+  .item
+    line-height: 0.4rem
+    text-align: center
+    color: $bgColor
+</style>
