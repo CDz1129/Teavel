3e65bde5d7a2c6aafd8e7f75040bfa983267ce5f
CDz <smartcdz@163.com>
2019-09-02 17:06:40 +0800

    search

--- src/views/city/components/Search.vue
+++ src/views/city/components/Search.vue
@@ -1,12 +1,53 @@
 <template>
+<div>
   <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+    <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名或拼音" />
   </div>
+  <div class="search-context">
+      <ul>
+          <li v-for="item in list" :key="item.id">{{item.name}}</li>
+      </ul>
+  </div>
+</div>
 </template>
 
 <script>
+import { setTimeout } from 'timers'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (!this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const searchList = []
+        for (const key in this.cities) {
+          if (this.cities.hasOwnProperty(key)) {
+            const element = this.cities[key]
+            element.forEach(value => {
+              if (value.spell.includes(this.keyword) ||
+                    value.name.includes(this.keyword)) {
+                searchList.push(value)
+              }
+            })
+          }
+        }
+        this.list = searchList
+      }, 60)
+    }
+  }
+
 }
 </script>
 
@@ -25,4 +66,13 @@ export default {
     text-align: center
     border-radius: 0.06rem
     color: #666
+.search-context
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background green
 </style>
